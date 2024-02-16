73d86e18019a6b9fbce9bdc6dd77cd0e229a5f81
Rishi <91426745+rishi998@users.noreply.github.com>
2024-02-16 04:37:55 +0000

    done

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountId=123123
+let accountname="Savings"
+var email="rishi@gmail.com"
+// prefer not to use var because of issue in blocm scope and functional scope 
+let password = 12345
+console.table([accountId, accountname, email, password])
---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,4 @@
+"use strict" //treat all JS code as newer version
+//alert(3+3) // we are using node js, not browser hence this wont work
+console.log(3+3);
+// code readability shoyuld be high
