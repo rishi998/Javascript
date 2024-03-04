90246b092f8fbd588dcdfbfb7187c60e98111a1c
Rishi <91426745+rishi998@users.noreply.github.com>
2024-03-04 13:49:55 +0000

    done

---
+++ .devcontainer/01_basics/04_maths.js
@@ -0,0 +1,18 @@
+const score = 400 
+// console.log(score+ 10)
+// console.log(Math.abs(-4))
+// // ceil takes values from the upper side
+// console.log(Math.ceil(4.6))
+// // floor takes the value on the lower side
+// console.log(Math.floor(4.9))
+
+// console.log(Math.min(4,3,6,8))
+// console.log(Math.max(4,2,6,8))
+
+// console.log(Math.random())
+// console.log(Math.random()*10+1)
+
+const max=20
+const min=10
+
+console.log(Math.floor(Math.random()*10+min))
