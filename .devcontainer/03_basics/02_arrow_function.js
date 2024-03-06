8534fd9c8c3c5cababad650d9722d82237f4fd26
Rishi <91426745+rishi998@users.noreply.github.com>
2024-03-06 12:16:49 +0000

    done

---
+++ .devcontainer/03_basics/02_arrow_function.js
@@ -0,0 +1,21 @@
+const user={
+    username:"Rishi",
+    price:99,
+    welcomeMessage:function(){
+        console.log(`${this.username}, welcome to the website!!`)
+    }
+}
+// this keyword works only inside an object in the scope
+// user.welcomeMessage()
+user.username="sam"
+// console.log(user.welcomeMessage())
+
+// arrow function
+// implicit return 
+const  addtwo=(num1, num2)=>(num1+num2) 
+
+// explicit return 
+const addtone(num1,num2)=>{
+    return num1+num2
+}
+console.log(addtwo(4,4))
