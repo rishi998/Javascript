7d0d8c629006a0d30c9121cb71424b277ed395f1
Rishi <91426745+rishi998@users.noreply.github.com>
2024-03-06 11:42:47 +0000

    done

--- .devcontainer/03_basics/01_functions.js
+++ .devcontainer/03_basics/01_functions.js
@@ -24,4 +24,65 @@ function loggedIn(username="User123"){
 }
 
 // console.log(loggedIn("Rishi"))
-console.log(loggedIn("Hero"))
+// console.log(loggedIn("Hero"))
+
+
+// the rest operator and spread operator
+function calculatePrice(val1, val2,...num1){
+    return num1
+}
+// console.log(calculatePrice(200,300,400,500))
+
+const user={
+    username:"rishi",
+    price:199
+}
+
+function handleuser(anyobject){
+    console.log(`${anyobject.username} is the username and price is ${anyobject.price}`)
+}
+// we are passing an object into the handle object function created above.
+
+// method 1::
+// handleuser(user)
+
+// Method 2::
+// handleuser({
+//     username:"sam",
+//     price:201
+// })
+
+const arr=[200, 400, 300, 50]
+function returnSecond(anyarr){
+    console.log(`${anyarr[1]} is the second element of the given array.`)
+}
+// method1::
+// returnSecond(arr)
+
+// method 2::
+// returnSecond([10,20,30,40])
+
+function one(){
+    const website="google"
+    function two(){
+        const username = "rishi"
+        console.log(website)
+    }
+    two()
+}
+// one()
+
+// +++++++++++++++++++++++++++++++++=interesting+++++++++++++++++++++++++++++++++++
+
+// a proper fucntion(addone) can be called from anywhere in the code
+// but a variable function(addtwo) can only be called after the declaration of the fucntion
+// console.log(addone(5))
+// console.log(addtwo(10))
+function addone(num){
+    return num+1
+}
+
+const addtwo=function(num){
+    return num+2
+}
+
